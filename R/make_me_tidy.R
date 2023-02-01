e979bed9328235483ecf309975da137b860281cd
Fernando Duarte <duarte@alum.mit.edu>
2023-02-01 10:46:31 -0500

    Clean make_me_tidy.R

--- R/make_me_tidy.R
+++ R/make_me_tidy.R
@@ -8,10 +8,3 @@
 hello_world <- function() {
   print("Hello World")
 }
-
-# usethis::use_github_action("lint")
-<<<<<<< HEAD
-# usethis::use_github_action("document")
-=======
-
->>>>>>> 7946a92 (usethis::use_github_action("lint"))
