460f6020704edb5b335b27e234799b43525a82b0
Fernando Duarte <duarte@alum.mit.edu>
2023-03-14 10:25:54 -0400

    Lint `hetreg.R`, `vfci_hetreg_tidiers.R`.

--- R/vfci_hetreg_tidiers.R
+++ R/vfci_hetreg_tidiers.R
@@ -9,12 +9,15 @@
 #' @template param_unused_dots
 #' @aliases het_tidiers
 #' @return Various statistics and post-estimation values for [hetreg()]
-#' @seealso [broom::tidy()], `nlme_tidiers()` from [`broom.mixed`](https://github.com/bbolker/broom.mixed)
+#' @seealso [broom::tidy()], `nlme_tidiers()` from
+#'  [`broom.mixed`](https://github.com/bbolker/broom.mixed)
 
 #' @export
-#' @describeIn tidy.het returns the same information as `nlme_tidiers()` from [`broom.mixed`](https://github.com/bbolker/broom.mixed)
-#' for the mean equation, and adds coefficients, standard errors, t-statistics and p-values for the variance equation
-tidy.het <- function(x, conf.int = FALSE, conf.level = 0.95, ...) {
+#' @describeIn tidy.het returns the same information as `nlme_tidiers()` from
+#' [`broom.mixed`](https://github.com/bbolker/broom.mixed)
+#' for the mean equation, and adds coefficients, standard errors, t-statistics
+#' and p-values for the variance equation
+tidy.het <- function(x, conf_int = FALSE, conf_level = 0.95, ...) {
   get_attr <- \(x) attr(x, "formula")
   idx <- list("modelStruct", "varStruct", \(x) purrr::map(x, get_attr))
   vareq_names <- purrr::pluck(x, !!!idx) %>%
@@ -23,22 +26,23 @@ tidy.het <- function(x, conf.int = FALSE, conf.level = 0.95, ...) {
     stringr::str_remove_all("~") %>%
     stringr::str_split(" ")
   vareq_coef <- 2 * attr(x$apVar, "Pars") %>% unname()
-  estimate <- std.error <- statistic <- NULL
+  estimate <- std_error <- statistic <- NULL
   result <- dplyr::tibble(
     term = c(vareq_names, "(Intercept)") %>% unlist(),
     estimate = vareq_coef, # \alpha coefficients of vol eq
-    std.error = 2 * sqrt(diag(x$apVar)) %>% unname(), # std.error of \alpha
-    statistic = estimate / std.error,
+    std_error = 2 * sqrt(diag(x$apVar)) %>% unname(), # std_error of \alpha
+    statistic = estimate / std_error,
     p.value = 2 * stats::pnorm(-abs(statistic))
   )
-  if (conf.int) {
-    ci <- stats::confint(x, level = conf.level)
+  if (conf_int) {
+    ci <- stats::confint(x, level = conf_level)
     result <- dplyr::left_join(result, ci, by = "term")
   }
   result
 }
 #' @export
-#' @describeIn tidy.het same as [`broom.mixed::glance.gls()`](https://github.com/bbolker/broom.mixed)
+#' @describeIn tidy.het same as
+#'  [`broom.mixed::glance.gls()`](https://github.com/bbolker/broom.mixed)
 glance.het <- function(x, ...) {
   x <- structure(x, class = c("gls"))
   broom.mixed::glance(x)
@@ -61,11 +65,13 @@ generics::tidy
 #' @importFrom generics glance
 #' @export
 #' @inherit generics::glance title return
-#' @seealso [`broom.mixed::glance.gls()`](https://github.com/bbolker/broom.mixed)
+#' @seealso
+#' [`broom.mixed::glance.gls()`](https://github.com/bbolker/broom.mixed)
 generics::glance
 
 #' @importFrom generics augment
 #' @export
 #' @inherit generics::augment title return
-#' @seealso [`broom.mixed::augment.gls()`](https://github.com/bbolker/broom.mixed)
+#' @seealso
+#'  [`broom.mixed::augment.gls()`](https://github.com/bbolker/broom.mixed)
 generics::augment
